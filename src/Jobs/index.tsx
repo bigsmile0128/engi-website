64de9912c850bb15c9cf7faf4171ac649013c308
bigsmile0128 <goldwebnike@gmail.com>
2022-03-17 23:53:36 -0700

    add recommended job

--- src/Jobs/index.tsx
+++ src/Jobs/index.tsx
@@ -3,20 +3,28 @@ import TimeEstimate from '../components/TimeEstimate';
 
 export default function Jobs() {
   return (
+    // TODO: make responsive
     <div className="flex flex-col p-24">
       <div className="flex items-start justify-between">
         <h1 className="text-white font-grifter text-6xl">
           <span className="md:block">Discover </span>
-          <span className="md:block">Jobs</span>
+          <span className="md:block">jobs</span>
         </h1>
         <div className="flex flex-col items-start">
-          <h3 className="font-grifter text-white">Recommended for you</h3>
-          <div className="bg-[#00000022] p-6 flex flex-col">
-            <div className="flex justify-between items-center w-64">
-              <span className="bg-orange-400 text-white rounded-xl text-xs px-4 py-1 flex">
+          <h3 className="font-grifter text-white mb-2">Recommended for you</h3>
+          {/* TODO: change to Link component */}
+          <div className="bg-[#00000022] hover:bg-[#44444422] cursor-pointer p-6 flex flex-col">
+            <div className="flex items-center justify-between">
+              <span className="bg-red-400 text-white rounded-xl text-xs px-4 py-1 flex">
                 Hot
               </span>
               <TimeEstimate duration="2 hours" />
+            </div>
+            <div className="flex items-center justify-between mt-8">
+              <span className="shrink-0 text-gray-300 mr-20 text-sm font-semibold">
+                Job Details component
+              </span>
+              <span className="font-grifter text-xl text-white">$540</span>
             </div>
           </div>
         </div>
