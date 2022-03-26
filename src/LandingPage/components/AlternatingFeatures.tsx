23ee4b08710a22093d13a0a8c602ab230ff64192
bigsmile0128 <goldwebnike@gmail.com>
2022-03-25 17:47:27 -0700

    make alternating features responsive

--- src/LandingPage/components/AlternatingFeatures.tsx
+++ src/LandingPage/components/AlternatingFeatures.tsx
@@ -14,37 +14,40 @@ export default function AlternatingFeatures({
   className,
 }: AlternatingFeaturesProps) {
   return (
-    // TODO: make responsive
     <div className={classNames('', className)}>
-      <div className="grid grid-cols-2 grid-flow-row-dense gap-24 items-center">
-        <div>
+      <div className="md:grid grid-cols-2 grid-flow-row-dense gap-24 items-center mx-6 md:mx-0">
+        <div className="mb-12 md:mb-0">
           <h2 className="font-grifter text-3xl mb-4">Figma Plugins</h2>
           <p className="text-gray-400">
             Cupcake ipsum dolor sit amet. Tootsie roll sesame snaps chocolate
             bar gummies tiramisu ice cream liquorice chocolate cake.
           </p>
         </div>
-        <div className="block">
-          <img src={cubes} alt="figma plugins" />
+        <div className="max-w-md md:max-w-none mx-auto">
+          <img
+            className="h-3/4 w-3/4 mx-auto"
+            src={cubes}
+            alt="figma plugins"
+          />
         </div>
       </div>
-      <div className="grid grid-cols-2 grid-flow-row-dense gap-24 items-center mt-24">
-        <div className="col-start-2">
+      <div className="md:grid grid-cols-2 grid-flow-row-dense gap-24 items-center mx-6 md:mx-0 mt-24">
+        <div className="mb-12 md:mb-0 col-start-2">
           <h2 className="font-grifter text-3xl mb-4">Website</h2>
           <p className="text-gray-400">
             Cupcake ipsum dolor sit amet. Tootsie roll sesame snaps chocolate
             bar gummies tiramisu ice cream liquorice chocolate cake.
           </p>
         </div>
-        <div className="relative col-start-1">
+        <div className="max-w-md md:max-w-none mx-auto relative col-start-1">
           <img src={ipad} alt="ipad website" />
           <div className="absolute -bottom-10 -left-20 h-50 w-50">
             <img className="w-auto h-60" src={iphone} alt="iphone website" />
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-2 grid-flow-row-dense gap-24 items-center mt-40">
-        <div className="">
+      <div className="md:grid grid-cols-2 grid-flow-row-dense gap-24 items-center mx-6 md:mx-0 mt-40">
+        <div className="mb-12 md:mb-0">
           <h2 className="font-grifter text-3xl mb-4">
             Clients &amp; Integrations
           </h2>
@@ -53,7 +56,7 @@ export default function AlternatingFeatures({
             bar gummies tiramisu ice cream liquorice chocolate cake.
           </p>
         </div>
-        <div className="">
+        <div className="max-w-md md:max-w-none mx-auto">
           <img src={blocks} alt="clients and integrations" />
         </div>
       </div>
