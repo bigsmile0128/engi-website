fcbbaa6e1a9197e613736e1e5fe46b01d3344950
bigsmile0128 <goldwebnike@gmail.com>
2022-05-08 14:14:29 -0700

    update env variable for build

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/:path*`, // proxy to back-end
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // proxy to back-end
       },
     ];
   },
