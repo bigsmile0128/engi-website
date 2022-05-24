922f4dfc18471a97901cfa8db3579f0f5369272a
bigsmile0128 <goldwebnike@gmail.com>
2022-05-23 17:44:11 -0700

    add social media to footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import classNames from 'classnames';
 
 import LogoSvg from './logo.svg';
+import LogoAltSvg from './logo_alt.svg';
+import { SiDiscord, SiLinkedin, SiTiktok, SiTwitter } from 'react-icons/si';
 
 interface FooterProps {
   className?: string;
@@ -12,12 +14,27 @@ export default function Footer({ className }: FooterProps) {
   return (
     <div
       className={classNames(
-        'bg-black flex items-center justify-center px-12 py-6',
+        'bg-[#070706bb] flex items-center justify-center px-12 py-16 xs:py-6',
         className
       )}
     >
-      <div className="w-full max-w-3xl lg:max-w-4xl flex items-center gap-x-12 sm:gap-x-16">
-        <LogoSvg className="h-8 fill-emerald-300 shrink-0" />
+      <div className="w-full max-w-3xl lg:max-w-4xl flex flex-col xs:flex-row items-center justify-between gap-x-12 sm:gap-x-16">
+        <LogoSvg className="h-8 fill-emerald-300 shrink-0 hidden xs:block" />
+        <LogoAltSvg className="h-10 mb-12 block xs:hidden" />
+        <div className="flex items-center gap-x-8">
+          <button className="p-2 text-2xl hover:text-gray-300">
+            <SiDiscord />
+          </button>
+          <button className="p-2 text-2xl hover:text-gray-300">
+            <SiTiktok />
+          </button>
+          <button className="p-2 text-2xl hover:text-gray-300">
+            <SiLinkedin />
+          </button>
+          <button className="p-2 text-2xl hover:text-gray-300">
+            <SiTwitter />
+          </button>
+        </div>
       </div>
     </div>
   );
