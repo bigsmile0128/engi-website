f7a6e6d462ccd188a38221ad1d4bf9e6ddc785f2
bigsmile0128 <goldwebnike@gmail.com>
2022-07-22 19:29:54 -0700

    test disabling ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,53 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-import Hero from 'components/home/Hero';
-import FigmaPreview from 'components/home/FigmaPreview';
-import Features from 'components/home/Features';
-import CalloutCards from 'components/home/CalloutCards';
-import BlockQuote from 'components/home/BlockQuote';
-import GlobalConnection from 'components/home/GlobalConnection';
-import AlternatingFeatures from 'components/home/AlternatingFeatures';
-import CallToAction from 'components/home/CallToAction';
+// import Hero from 'components/home/Hero';
+const Hero = dynamic(() => import('components/home/Hero'), { ssr: false });
+// import FigmaPreview from 'components/home/FigmaPreview';
+const FigmaPreview = dynamic(() => import('components/home/FigmaPreview'), {
+  ssr: false,
+});
+// import Features from 'components/home/Features';
+const Features = dynamic(() => import('components/home/Features'), {
+  ssr: false,
+});
+// import CalloutCards from 'components/home/CalloutCards';
+const CalloutCards = dynamic(() => import('components/home/CalloutCards'), {
+  ssr: false,
+});
+// import BlockQuote from 'components/home/BlockQuote';
+const BlockQuote = dynamic(() => import('components/home/BlockQuote'), {
+  ssr: false,
+});
+// import GlobalConnection from 'components/home/GlobalConnection';
+const GlobalConnection = dynamic(
+  () => import('components/home/GlobalConnection'),
+  { ssr: false }
+);
+// import AlternatingFeatures from 'components/home/AlternatingFeatures';
+const AlternatingFeatures = dynamic(
+  () => import('components/home/AlternatingFeatures'),
+  { ssr: false }
+);
+// import CallToAction from 'components/home/CallToAction';
+const CallToAction = dynamic(() => import('components/home/CallToAction'), {
+  ssr: false,
+});
+// import Stats from 'components/home/Statistics';
+const Stats = dynamic(() => import('components/home/Statistics'), {
+  ssr: false,
+});
+// import WorldOfEngineers from 'components/home/WorldOfEngineers';
+const WorldOfEngineers = dynamic(
+  () => import('components/home/WorldOfEngineers'),
+  { ssr: false }
+);
+// import WhosUsingEngi from 'components/home/WhosUsingEngi';
+const WhosUsingEngi = dynamic(() => import('components/home/WhosUsingEngi'), {
+  ssr: false,
+});
 import { isBeta } from 'utils';
-import Stats from 'components/home/Statistics';
-import WorldOfEngineers from 'components/home/WorldOfEngineers';
-import WhosUsingEngi from 'components/home/WhosUsingEngi';
 
 const Home: NextPage = () => {
   return (
