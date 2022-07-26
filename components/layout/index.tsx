fa7772c297cf4d6ce73cc0fb50d8497041ba504a
bigsmile0128 <goldwebnike@gmail.com>
2022-07-26 13:39:24 -0400

    tested with png

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
@@ -13,11 +12,12 @@ export default function Layout({ children }: LayoutProps) {
     <main className="relative min-h-screen flex flex-col">
       <div className="absolute w-full h-full -z-40">
         <Image
-          src="/img/landing.svg"
+          src="/img/website-bg.png"
           alt="bgImage"
           layout="fill"
           objectFit="cover"
           loading="lazy"
+          sizes="100vw"
         />
       </div>
       <Navbar />
