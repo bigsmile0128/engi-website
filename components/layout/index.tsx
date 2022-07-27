f02f2dccd52ef9957f7e062bd58354e26527d5fe
bigsmile0128 <goldwebnike@gmail.com>
2022-07-27 08:26:04 -0400

    fixed bg issue

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image';
+import cn from 'classnames';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import styles from './layout.module.css';
 
 type LayoutProps = {
   children?: React.ReactNode;
@@ -8,18 +9,13 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    //bg-landing bg-cover cause performance issue
-    <main className="relative min-h-screen flex flex-col">
-      <div className="absolute w-full h-full -z-40">
-        <Image
-          src="/img/website-bg.png"
-          alt="bgImage"
-          layout="fill"
-          objectFit="cover"
-          loading="lazy"
-          sizes="100vw"
-        />
-      </div>
+    <main
+      className={cn(
+        'relative min-h-screen flex flex-col test-bg',
+        styles.main_bg
+      )}
+    >
+      <div className="absolute w-full h-full -z-40"></div>
       <Navbar />
       <div className="overflow-hidden">{children}</div>
       <Footer className="mt-auto" />
