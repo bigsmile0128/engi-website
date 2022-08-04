49e1ed55e3121170dbab5956abb55b57e3755171
bigsmile0128 <goldwebnike@gmail.com>
2022-08-03 22:08:07 -0700

    update dockerfile to use custom server

--- server.js
+++ server.js
@@ -7,19 +7,24 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  server.use(express.json());
-  server.use(express.urlencoded({ extended: true }));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
 
-  server.use('/api/jobs', jobs);
+    server.use('/api/jobs', jobs);
 
-  server.all('*', (req, res) => {
-    return handle(req, res);
+    server.all('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, () => {
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
