8bc48552344ef7503aa15e1ffdfdb90e4ee1e056
bigsmile0128 <goldwebnike@gmail.com>
2022-08-10 09:00:35 -0400

    replaced block image

--- components/home/AlternatingFeatures.tsx
+++ components/home/AlternatingFeatures.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import Image from 'next/image';
-import blocks from '../../public/img/home/blocks.png';
+import blocks from 'public/img/home/blocks.png';
 import PlaceholderButton from './PlaceholderButton';
 
 interface AlternatingFeaturesProps {
