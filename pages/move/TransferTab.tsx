82255e0c79756378d2eb4a22e00058aad9f33fb6
bigsmile0128 <goldwebnike@gmail.com>
2022-09-06 19:18:11 +0900

    fixes ENGIN-322 add note on transfer tab

--- pages/move/TransferTab.tsx
+++ pages/move/TransferTab.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import WalletInput from './WalletInput';
+import Input from 'components/Input';
 
 type TransferTabProps = {
   className?: string;
@@ -10,6 +11,15 @@ export default function TransferTab({ className }: TransferTabProps) {
   return (
     <div className={classNames('flex flex-col', className)}>
       <WalletInput />
+      <label htmlFor="note" className="mt-8 font-bold text-xl">
+        Note
+      </label>
+      <Input
+        className="mt-2"
+        type="text"
+        name="note"
+        placeholder="Write an optional message"
+      />
     </div>
   );
 }
