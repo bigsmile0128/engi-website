909e78f8edec8ecd20adb38b5384068b026ce232
bigsmile0128 <goldwebnike@gmail.com>
2022-09-13 01:45:10 -0400

    added NextImage optimization for stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@
 // https://github.com/tailwindlabs/tailwindcss/issues/6314
 import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
 import '!style-loader!css-loader!postcss-loader!../styles/globals.css';
+import * as NextImage from 'next/image';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,3 +13,10 @@ export const parameters = {
     },
   },
 };
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
---
+++ components/stories/pages/Contact.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ContactUs from '../../../pages/contact';
+
+export default {
+  title: 'Contact Page',
+  component: ContactUs,
+};
+
+const Template = (args) => <ContactUs {...args} />;
+
+// export const Default  = Template.bind({});
+// Default.args = {}
