943d4789016147efe024f1f3279e1d9f8fbf4ee4
bigsmile0128 <goldwebnike@gmail.com>
2022-09-22 03:32:03 -0400

    changed settings

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
+import Meta from '~/components/global/Meta/Meta';
 
 const Settings: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Engi</title>
-      </Head>
+      <Meta title="Engi" />
       settings page
     </div>
   );
