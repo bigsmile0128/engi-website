2854540f7b17ee05eeec20a4415c4808c73e9a3d
bigsmile0128 <goldwebnike@gmail.com>
2022-09-28 02:36:51 -0400

    added Dropzone

--- src/components/modules/settings/UploadAvatar/Dropzone.tsx
+++ src/components/modules/settings/UploadAvatar/Dropzone.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 function Dropzone() {
-  return <div>Dropzone</div>;
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+    console.log('files===>', acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className="flex justify-center border border-dashed border-[white]/30 py-20"
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <div>
+          <div className="flex w-fit border border-[white]/60 backdrop-blur-[11px] bg-[#161b28]/30 p-4 m-auto mb-6">
+            <Image
+              src="/img/settings/add-image.svg"
+              height={27}
+              width={27}
+              alt="add image"
+            />
+          </div>
+          <p className="flex flex-col">
+            Drag and drop an image, or Browse
+            <span className="text-xs text-secondary">
+              1200x1200 or higher recommended. Max 10MB each
+            </span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Dropzone;
--- src/components/modules/settings/UploadAvatar/UploadAvatar.tsx
+++ src/components/modules/settings/UploadAvatar/UploadAvatar.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import Button from '~/components/Button';
+import Dropzone from './Dropzone';
 
 function UploadAvatar() {
-  return <div>UploadAvatar</div>;
+  const handleClickSave = () => {};
+
+  return (
+    <div className="p-8">
+      <h2 className="font-grifter font-bold text-3xl">Upload your Avatar</h2>
+      <h6>Help us personalize avatar.</h6>
+      <div className="my-12">
+        <Dropzone />
+      </div>
+      <div className="text-center">
+        <Button onClick={handleClickSave} variant="primary" className="w-2/5">
+          Save
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default UploadAvatar;
