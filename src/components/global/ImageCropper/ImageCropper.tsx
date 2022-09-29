16e48c8d60d6d01be10a9fac38b87d83d6520b9d
bigsmile0128 <goldwebnike@gmail.com>
2022-09-29 10:28:03 -0400

    added styles

--- src/components/global/ImageCropper/ImageCropper.tsx
+++ src/components/global/ImageCropper/ImageCropper.tsx
@@ -1,27 +1,28 @@
 import { useState, useCallback } from 'react';
 import Cropper from 'react-easy-crop';
-
+import { Point, Area } from 'react-easy-crop/types';
 function ImageCropper() {
-  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
+
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
     console.log(croppedArea, croppedAreaPixels);
   }, []);
 
   return (
-    <div>
-      <div className="crop-container">
+    <div className="relative h-[350px] w-[350px] m-auto">
+      <div className="absolute top-0 left-0 right-0 bottom-5">
         <Cropper
           image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
           crop={crop}
           zoom={zoom}
-          aspect={4 / 3}
+          aspect={1}
           onCropChange={setCrop}
           onCropComplete={onCropComplete}
           onZoomChange={setZoom}
         />
       </div>
-      <div className="controls">
+      <div className="flex items-center absolute bottom-0 -translate-x-2/4">
         <input
           type="range"
           value={zoom}
