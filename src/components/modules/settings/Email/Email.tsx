8f9b978c308581e8c1fa40cd9111d7b7b6e1758d
bigsmile0128 <goldwebnike@gmail.com>
2022-10-02 21:02:33 -0400

    added Email verification mockup

--- src/components/modules/settings/Email/Email.tsx
+++ src/components/modules/settings/Email/Email.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import Button from '~/components/global/Button/Button';
 
 function Email() {
-  return <div>Email</div>;
+  const handleVerifyEmail = () => {};
+
+  return (
+    <div className="p-8 flex flex-col">
+      <h2 className="font-grifter font-bold text-3xl">Verify your email</h2>
+      <h6 className="text-secondary">
+        From your mobile device, tap the button below to confirm:
+      </h6>
+      <div className="text-center  my-12">
+        <Button onClick={handleVerifyEmail} variant="primary" className="w-2/5">
+          Verify Email Address
+        </Button>
+      </div>
+      <p className="text-secondary text-center">
+        If you didn’t request this email. there’s nothing to worry about - you
+        can safely ignore it
+      </p>
+    </div>
+  );
 }
 
 export default Email;
--- src/components/modules/settings/UploadAvatar/UploadAvatar.tsx
+++ src/components/modules/settings/UploadAvatar/UploadAvatar.tsx
@@ -70,7 +70,7 @@ function UploadAvatar() {
   return (
     <div className="p-8">
       <h2 className="font-grifter font-bold text-3xl">Upload your Avatar</h2>
-      <h6>Help us personalize your avatar.</h6>
+      <h6 className="text-secondary">Help us personalize your avatar.</h6>
       {hasUploaded ? renderImageCropper() : renderDropzone()}
     </div>
   );
