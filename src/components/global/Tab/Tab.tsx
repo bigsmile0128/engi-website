4dcdcf99c88794cc3e8a7e8d10c6988852680133
bigsmile0128 <goldwebnike@gmail.com>
2022-10-02 21:22:08 -0400

    added Tab mock

---
+++ src/components/global/Tab/Tab.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Tab as HeadlessTab } from '@headlessui/react';
+import cn from 'classnames';
+import { Children, ReactNode, useState } from 'react';
+
+interface Props {
+  children: ReactNode;
+  className?: string;
+  defaultIndex?: number;
+  onChange?: (index: number) => void;
+  tabLabels: Array<ReactNode>;
+  tabListClassname?: string;
+}
+
+function Tab({
+  tabLabels,
+  children,
+  onChange,
+  defaultIndex = 0,
+  className,
+  tabListClassname,
+}: Props) {
+  const [selectedTab, setSelectTab] = useState<number>(-1);
+  const handleTabChange = (index: number) => {
+    onChange && onChange(index);
+    setSelectTab(index);
+  };
+
+  const rootClasses = cn(className, 'w-full');
+  const tabListClasses = cn(
+    tabListClassname,
+    'flex p-1 space-x-1 bg-primary-white/[0.14]'
+  );
+
+  return (
+    <div className={rootClasses}>
+      <HeadlessTab.Group
+        onChange={handleTabChange}
+        selectedIndex={selectedTab}
+        defaultIndex={defaultIndex}
+      >
+        <HeadlessTab.List className={tabListClasses}>
+          {tabLabels.map((element, index) => (
+            <HeadlessTab
+              key={index}
+              className={({ selected }) =>
+                cn(
+                  'py-2.5 text-sm leading-5 font-medium focus:outline-none',
+                  selected
+                    ? 'bg-primary-green text-secondary-bg'
+                    : 'text-primary-white hover:bg-white/[0.12] hover:text-blue-100'
+                )
+              }
+            >
+              {element}
+            </HeadlessTab>
+          ))}
+        </HeadlessTab.List>
+        <HeadlessTab.Panels className="mt-4">
+          {Children.map(children, (child, idx) => (
+            <HeadlessTab.Panel key={idx} className={cn('focus:outline-none')}>
+              {child}
+            </HeadlessTab.Panel>
+          ))}
+        </HeadlessTab.Panels>
+      </HeadlessTab.Group>
+    </div>
+  );
+}
+
+export default Tab;
--- src/components/modules/settings/PersonalInfo/PersonalInfo.tsx
+++ src/components/modules/settings/PersonalInfo/PersonalInfo.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
 function PersonalInfo() {
-  return <div>PersonalInfo</div>;
+  return (
+    <div className="p-8">
+      <h2 className="font-grifter font-bold text-3xl">Upload your Avatar</h2>
+      <h6 className="text-secondary">Help us personalize your avatar.</h6>
+    </div>
+  );
 }
 
 export default PersonalInfo;
