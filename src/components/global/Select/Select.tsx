82b4d40dde0c1f0d7986fdc6b25519fd49d4718a
bigsmile0128 <goldwebnike@gmail.com>
2022-10-04 08:10:09 -0400

    added JobPreference

--- src/components/global/Select/Select.tsx
+++ src/components/global/Select/Select.tsx
@@ -2,20 +2,37 @@ import { ReactNode, useState } from 'react';
 
 interface Props {
   items: Array<ReactNode>;
-  onSelect: () => void;
+  onSelect: (values: Array<number>) => void;
   multi?: boolean;
 }
 
 function Selection({ items, onSelect, multi = false }: Props) {
-  const [selectedItems, setSelectedItems] = useState();
+  const [selectedItems, setSelectedItems] = useState<Array<number>>([]);
 
-  const handleClick = () => {};
+  const handleClick = (index: number) => () => {
+    let temp = [...selectedItems];
+
+    if (multi) {
+      const foundIndex = temp.indexOf(index);
+
+      if (foundIndex > -1) {
+        temp.splice(foundIndex, 1);
+      } else {
+        temp = [...selectedItems, index];
+      }
+    } else {
+      temp = [index];
+    }
+
+    onSelect(temp);
+    setSelectedItems(temp);
+  };
 
   return (
-    <div>
+    <div className="flex flex-wrap justify-center gap-5">
       {items.map((item, index) => {
         return (
-          <div key={`multi_select_${index}`} onClick={handleClick}>
+          <div key={`multi_select_${index}`} onClick={handleClick(index)}>
             {item}
           </div>
         );
--- src/components/modules/settings/JobPreference/JobPreference.tsx
+++ src/components/modules/settings/JobPreference/JobPreference.tsx
@@ -1,6 +1,12 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import Select from '~/components/global/Select/Select';
 import JavaScriptIcon from 'public/img/settings/javascript.svg';
+import CPlusPlusIcon from 'public/img/settings/c-plus-plus.svg';
+import PythonIcon from 'public/img/settings/python.svg';
+import HtmlIcon from 'public/img/settings/html5.svg';
+import RustIcon from 'public/img/settings/rust.svg';
+import TypescriptIcon from 'public/img/settings/typescript.svg';
+import ReactIcon from 'public/img/settings/reactjs.svg';
 
 type SelectItem = {
   icon: ReactNode;
@@ -13,43 +19,49 @@ const itemData: SelectItem[] = [
     title: 'Javascript',
   },
   {
-    icon: <JavaScriptIcon width={33} height={33} />,
-    title: 'Javascript',
+    icon: <CPlusPlusIcon width={33} height={33} />,
+    title: 'C++',
   },
   {
-    icon: <JavaScriptIcon width={33} height={33} />,
-    title: 'Javascript',
+    icon: <PythonIcon width={33} height={33} />,
+    title: 'Python',
   },
   {
-    icon: <JavaScriptIcon width={33} height={33} />,
-    title: 'Javascript',
+    icon: <HtmlIcon width={33} height={33} />,
+    title: 'HTML',
   },
   {
-    icon: <JavaScriptIcon width={33} height={33} />,
-    title: 'Javascript',
+    icon: <RustIcon width={33} height={33} />,
+    title: 'Rust',
   },
   {
-    icon: <JavaScriptIcon width={33} height={33} />,
-    title: 'Javascript',
+    icon: <TypescriptIcon width={33} height={33} />,
+    title: 'Typescript',
   },
   {
-    icon: <JavaScriptIcon width={33} height={33} />,
-    title: 'Javascript',
+    icon: <ReactIcon width={33} height={33} />,
+    title: 'React',
   },
 ];
 
 function JobPreference() {
+  const [selectedLangs, setSelectedLangs] = useState<Array<number>>([]);
   const renderItems = () => {
     return itemData.map(({ icon, title }) => (
       <div
         key={title}
         className="flex justify-between items-center border border-white/20 px-4 py-3"
       >
-        <div>{icon}</div>
+        <div className="mr-2">{icon}</div>
         <div className="text-xl">{title}</div>
       </div>
     ));
   };
+
+  const handleSelect = (values: Array<number>) => {
+    setSelectedLangs(values);
+  };
+
   return (
     <div className="p-8">
       <h2 className="font-grifter font-bold text-3xl">
@@ -58,7 +70,7 @@ function JobPreference() {
       <h6 className="text-secondary">
         Help us personalize your job recommendations.
       </h6>
-      <Select items={renderItems()} onSelect={() => {}} multi />
+      <Select items={renderItems()} onSelect={handleSelect} multi />
     </div>
   );
 }
