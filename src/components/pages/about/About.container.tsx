861f5b1bdb6918cfb20a7798c330b0f58ce919aa
bigsmile0128 <goldwebnike@gmail.com>
2022-10-06 05:58:30 -0400

    seperated out of member

--- src/components/pages/about/About.container.tsx
+++ src/components/pages/about/About.container.tsx
@@ -15,53 +15,14 @@ import BlockQuote from '~/components/pages/home/BlockQuote';
 import GridPattern from '~/components/global/GridPattern/GridPattern';
 import TeamMember from '~/components/pages/about/TeamMember';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
+import BulbIcon from '~/components/global/icons/bulb.svg';
+import RocketIcon from '~/components/global/icons/rocket.svg';
+
+import { members } from './About.utils';
 
 interface AboutUsPageProps {
   className?: string;
 }
-
-const members = [
-  {
-    name: 'Garrett',
-    role: 'CEO',
-    companies: ['Apple'],
-  },
-  {
-    name: 'Jay',
-    role: 'Software Engineer',
-    companies: ['Google'],
-  },
-  {
-    name: 'Mike',
-    role: 'Software Engineer',
-    companies: ['Google'],
-  },
-  {
-    name: 'Chris',
-    role: 'Software Engineer',
-    companies: ['Apple'],
-  },
-  {
-    name: 'Chris',
-    role: 'Software Engineer',
-    companies: ['Google'],
-  },
-  {
-    name: 'Georgios',
-    role: 'Software Engineer',
-    companies: ['Google'],
-  },
-  {
-    name: 'Mark',
-    role: 'Software Engineer',
-    companies: ['Google'],
-  },
-  {
-    name: 'Thomas',
-    role: 'Software Engineer',
-    companies: ['Google'],
-  },
-];
 
 function AboutUsPageContainer({ className }: AboutUsPageProps) {
   const [, setSelectedIndex] = useState(0);
@@ -159,18 +120,7 @@ function AboutUsPageContainer({ className }: AboutUsPageProps) {
           <div className="flex flex-col sm:flex-row items-center gap-x-12 gap-y-24">
             <div className="flex flex-col items-center gap-y-8">
               <div className="border border-gray-500 h-20 w-20 flex items-center justify-center bg-gray-800/60 z-10">
-                <svg
-                  width="28"
-                  height="36"
-                  viewBox="0 0 28 36"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.3333 27H7.23493C6.73993 24.8783 4.5066 23.1433 3.58993 22C2.02025 20.0388 1.03651 17.6741 0.75204 15.1782C0.467568 12.6824 0.893934 10.157 1.98202 7.89286C3.07011 5.62876 4.77566 3.71808 6.90219 2.38093C9.02873 1.04378 11.4897 0.334559 14.0017 0.334961C16.5137 0.335363 18.9745 1.04537 21.1006 2.3832C23.2267 3.72103 24.9317 5.63226 26.019 7.89671C27.1064 10.1612 27.5319 12.6867 27.2467 15.1825C26.9614 17.6782 25.9769 20.0426 24.4066 22.0033C23.4899 23.145 21.2599 24.88 20.7649 27H15.6666V18.6666H12.3333V27ZM20.6666 30.3333V32C20.6666 32.884 20.3154 33.7319 19.6903 34.357C19.0652 34.9821 18.2173 35.3333 17.3333 35.3333H10.6666C9.78255 35.3333 8.9347 34.9821 8.30958 34.357C7.68446 33.7319 7.33327 32.884 7.33327 32V30.3333H20.6666Z"
-                    fill="white"
-                  />
-                </svg>
+                <BulbIcon className="w-7" />
               </div>
               <h3 className="font-bold text-lg text-green-primary">
                 Empowering
@@ -181,18 +131,7 @@ function AboutUsPageContainer({ className }: AboutUsPageProps) {
             </div>
             <div className="flex flex-col items-center gap-y-8">
               <div className="border border-gray-500 h-20 w-20 flex items-center justify-center bg-gray-800/60 z-10">
-                <svg
-                  width="28"
-                  height="38"
-                  viewBox="0 0 28 38"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8.16341 31.3334H19.8367C18.7341 33.9706 16.6373 36.0675 14.0001 37.1701C11.3629 36.0675 9.26602 33.9706 8.16341 31.3334ZM24.0001 22.6751L27.3334 26.4551V29.6667H0.666748V26.4551L4.00008 22.6751V13.0001C4.00008 7.19505 8.17341 2.25505 14.0001 0.425049C19.8267 2.25505 24.0001 7.19505 24.0001 13.0001V22.6751ZM14.0001 16.3334C14.8841 16.3334 15.732 15.9822 16.3571 15.3571C16.9822 14.732 17.3334 13.8841 17.3334 13.0001C17.3334 12.116 16.9822 11.2682 16.3571 10.643C15.732 10.0179 14.8841 9.66672 14.0001 9.66672C13.116 9.66672 12.2682 10.0179 11.6431 10.643C11.0179 11.2682 10.6667 12.116 10.6667 13.0001C10.6667 13.8841 11.0179 14.732 11.6431 15.3571C12.2682 15.9822 13.116 16.3334 14.0001 16.3334Z"
-                    fill="white"
-                  />
-                </svg>
+                <RocketIcon className="w-7" />
               </div>
               <h3 className="font-bold text-lg text-green-primary">
                 Accelerating
@@ -203,18 +142,7 @@ function AboutUsPageContainer({ className }: AboutUsPageProps) {
             </div>
             <div className="flex flex-col items-center gap-y-8">
               <div className="border border-gray-500 h-20 w-20 flex items-center justify-center bg-gray-800/60 z-10">
-                <svg
-                  width="28"
-                  height="36"
-                  viewBox="0 0 28 36"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.3333 27H7.23493C6.73993 24.8783 4.5066 23.1433 3.58993 22C2.02025 20.0388 1.03651 17.6741 0.75204 15.1782C0.467568 12.6824 0.893934 10.157 1.98202 7.89286C3.07011 5.62876 4.77566 3.71808 6.90219 2.38093C9.02873 1.04378 11.4897 0.334559 14.0017 0.334961C16.5137 0.335363 18.9745 1.04537 21.1006 2.3832C23.2267 3.72103 24.9317 5.63226 26.019 7.89671C27.1064 10.1612 27.5319 12.6867 27.2467 15.1825C26.9614 17.6782 25.9769 20.0426 24.4066 22.0033C23.4899 23.145 21.2599 24.88 20.7649 27H15.6666V18.6666H12.3333V27ZM20.6666 30.3333V32C20.6666 32.884 20.3154 33.7319 19.6903 34.357C19.0652 34.9821 18.2173 35.3333 17.3333 35.3333H10.6666C9.78255 35.3333 8.9347 34.9821 8.30958 34.357C7.68446 33.7319 7.33327 32.884 7.33327 32V30.3333H20.6666Z"
-                    fill="white"
-                  />
-                </svg>
+                <BulbIcon className="w-7" />
               </div>
               <h3 className="font-bold text-lg text-green-primary">
                 Disrupting
---
+++ src/components/pages/about/About.utils.ts
@@ -0,0 +1,48 @@
+interface CompanyMemeber {
+  companies?: Array<string>;
+  name: string;
+  role: string;
+}
+
+export const members: Array<CompanyMemeber> = [
+  {
+    name: 'Garrett',
+    role: 'CEO',
+    companies: ['Apple'],
+  },
+  {
+    name: 'Jay',
+    role: 'Software Engineer',
+    companies: ['Google'],
+  },
+  {
+    name: 'Mike',
+    role: 'Software Engineer',
+    companies: ['Google'],
+  },
+  {
+    name: 'Chris',
+    role: 'Software Engineer',
+    companies: ['Apple'],
+  },
+  {
+    name: 'Chris',
+    role: 'Software Engineer',
+    companies: ['Google'],
+  },
+  {
+    name: 'Georgios',
+    role: 'Software Engineer',
+    companies: ['Google'],
+  },
+  {
+    name: 'Mark',
+    role: 'Software Engineer',
+    companies: ['Google'],
+  },
+  {
+    name: 'Thomas',
+    role: 'Software Engineer',
+    companies: ['Google'],
+  },
+];
