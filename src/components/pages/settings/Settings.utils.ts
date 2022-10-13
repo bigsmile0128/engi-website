cd14db1ee15129fac656b233f5b13446e69af7f6
bigsmile0128 <goldwebnike@gmail.com>
2022-10-13 08:01:31 -0400

    removed password and payment

--- src/components/pages/settings/Settings.utils.ts
+++ src/components/pages/settings/Settings.utils.ts
@@ -1,14 +1,11 @@
 import { AiOutlineUser } from 'react-icons/ai';
 import { RiExchangeLine, RiTerminalFill } from 'react-icons/ri';
 import { BsChatLeftDots } from 'react-icons/bs';
-import DollarIcon from 'public/img/settings/dollar.svg';
 
 export enum SETTINGS_TAB {
   EMAIL = 'email',
   JOB_PREF = 'job_preference',
   NOTIFICATION = 'notification',
-  PASSWORD = 'password',
-  PAYMENT = 'payment',
   PERSONAL_INFO = 'personal_info',
   UPLOAD_AVATAR = 'upload_avatar',
 }
@@ -31,11 +28,6 @@ export const SETTINGS_LINKS: LinkItem[] = [
     icon: AiOutlineUser,
   },
   {
-    key: SETTINGS_TAB.PASSWORD,
-    title: 'Password',
-    icon: RiExchangeLine,
-  },
-  {
     key: SETTINGS_TAB.EMAIL,
     title: 'Email',
     icon: BsChatLeftDots,
@@ -44,11 +36,6 @@ export const SETTINGS_LINKS: LinkItem[] = [
     key: SETTINGS_TAB.NOTIFICATION,
     title: 'Notifications',
     icon: RiExchangeLine,
-  },
-  {
-    key: SETTINGS_TAB.PAYMENT,
-    title: 'Payment details',
-    icon: DollarIcon,
   },
   {
     key: SETTINGS_TAB.JOB_PREF,
@@ -61,8 +48,6 @@ export const mapSettingsTabToTitle: Record<SETTINGS_TAB, string> = {
   [SETTINGS_TAB.EMAIL]: 'Verify your email',
   [SETTINGS_TAB.JOB_PREF]: 'What languages does your company use?',
   [SETTINGS_TAB.NOTIFICATION]: 'Notification Settings',
-  [SETTINGS_TAB.PASSWORD]: 'Password settings',
-  [SETTINGS_TAB.PAYMENT]: 'Payment settings',
   [SETTINGS_TAB.PERSONAL_INFO]: 'Update your account info',
   [SETTINGS_TAB.UPLOAD_AVATAR]: 'Upload your avatar',
 };
