d504afa28ac06dcba2d87be4970e7ae531e085f6
bigsmile0128 <goldwebnike@gmail.com>
2022-11-04 08:53:19 +0200

    fix: don't expose sendgrid key and add guard for production

--- src/utils/sendgrid/index.ts
+++ src/utils/sendgrid/index.ts
@@ -3,15 +3,27 @@ import client from '@sendgrid/client';
 import { ClientRequest } from '@sendgrid/client/src/request';
 import { HttpMethod } from '@sendgrid/helpers/classes/request';
 
-sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY || '');
-client.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY || '');
+function initialize(client) {
+  const apiKey = process.env.SENDGRID_API_KEY
+
+  if(!apiKey && process.env.NODE_ENV === 'production') {
+    throw new Error('Sendgrid key is not defined')
+  }
+
+  client.setApiKey(apiKey);
+}
 
 export async function sendMail(msg: MailDataRequired): Promise<ClientResponse> {
+  initialize(sgMail);
+
   const [response] = await sgMail.send(msg);
+
   return response;
 }
 
 export async function upsertContact(data): Promise<ClientResponse> {
+  initialize(client);
+
   const request: ClientRequest = {
     url: '/v3/marketing/contacts',
     method: 'PUT' as HttpMethod,
@@ -19,5 +31,6 @@ export async function upsertContact(data): Promise<ClientResponse> {
   };
 
   const [response] = await client.request(request);
+  
   return response;
 }
