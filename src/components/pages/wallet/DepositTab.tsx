2c0c1d6b5fceaa09bcaa044a13ebef9e36a0294b
bigsmile0128 <goldwebnike@gmail.com>
2023-08-02 16:56:08 +0200

    fixes ENGIN-1276 update move engi design

--- src/components/pages/wallet/DepositTab.tsx
+++ src/components/pages/wallet/DepositTab.tsx
@@ -1,24 +1,23 @@
 import classNames from 'classnames';
 import { Dispatch } from 'react';
-import { PreviewMoveEngi } from '~/utils/exchange/types';
 import BuyEngi from '~/components/pages/wallet/BuyEngi';
 import { useUser } from '~/utils/contexts/userContext';
 
 type DepositTabProps = {
   className?: string;
-  setPreviewMove: Dispatch<PreviewMoveEngi>;
+  setDepositAmount: Dispatch<number>;
 };
 
 export default function DepositTab({
   className,
-  setPreviewMove,
+  setDepositAmount,
 }: DepositTabProps) {
   const { user } = useUser();
 
   return (
     <div className={classNames('flex flex-col', className)}>
       {user?.walletId && (
-        <BuyEngi account={user.walletId} setPreviewMove={setPreviewMove} />
+        <BuyEngi account={user.walletId} setDepositAmount={setDepositAmount} />
       )}
     </div>
   );
