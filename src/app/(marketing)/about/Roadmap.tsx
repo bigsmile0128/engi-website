e467a29af62e475b16ae05bcf2b9a220262b8422
bigsmile0128 <goldwebnike@gmail.com>
2023-08-10 13:50:42 -0400

    Update about page roadmap

--- src/app/(marketing)/about/Roadmap.tsx
+++ src/app/(marketing)/about/Roadmap.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
+import { IoCheckmarkSharp } from 'react-icons/io5';
 
 type RoadmapProps = {
   className?: string;
   items: {
     color?: 'green-primary' | 'orange-primary' | 'purple-primary';
     date: string;
+    milestone?: boolean;
     subtitle?: string;
     title: string;
   }[];
@@ -14,40 +16,43 @@ type RoadmapProps = {
 export default function Roadmap({ className, items }: RoadmapProps) {
   return (
     // inline-block in wrapper to make width adjust to width of elements inside
-    // needed for scrollbar to exceed screen width
     <div className={classNames('relative inline-block', className)}>
       <div className="relative flex items-center px-8 tablet:px-24">
-        {items.map(({ date, title, subtitle, color }, i) => {
-          let translateX: string;
-          if (i === 0) {
-            translateX = '0';
-          } else if (i === 1) {
-            translateX = '-50%';
-          } else {
-            translateX = `-${Math.floor((i + 1) / 2) * 50}%`;
-          }
-
-          const translateY = i % 2 === 0 ? '-50%' : '50%';
+        {items.map(({ date, title, subtitle, color, milestone }, i) => {
           return (
             <div
               key={i}
               style={{
-                transform: `translate(${translateX}, ${translateY})`,
+                transform: `translateY(${i % 2 === 0 ? '-50%' : '50%'})`,
               }}
             >
               <div
                 key={i}
                 className={classNames(
-                  'relative flex',
+                  'relative flex w-full',
                   i % 2 === 0 ? 'flex-col' : 'flex-col-reverse'
                 )}
               >
                 <div
                   className={classNames(
-                    'relative p-4 tablet:p-8 box-shadow flex flex-col bg-[#161B28]/30'
+                    'relative p-4 tablet:p-8 box-shadow flex flex-col bg-[#161B28]/30 w-full',
+                    {
+                      [`border-${color}`]: milestone,
+                    }
                   )}
                   style={{
-                    boxShadow: 'inset 0px 0px 0px 1px rgba(255,255,255,0.6)',
+                    ...(milestone
+                      ? {
+                          borderWidth: '3px',
+                          borderStyle: 'solid',
+                          borderLeft: 0,
+                        }
+                      : {
+                          boxShadow:
+                            'inset 0px 0px 0px 1px rgba(255,255,255,0.6)',
+                        }),
+                    width: 'max-content',
+                    maxWidth: 300,
                   }}
                   key={i}
                 >
@@ -59,7 +64,7 @@ export default function Roadmap({ className, items }: RoadmapProps) {
                       'bg-purple-primary': color === 'purple-primary',
                     })}
                   />
-                  <div className="flex flex-col gap-2 tablet:gap-4">
+                  <div className="flex flex-col gap-2 tablet:gap-4 w-full">
                     <span
                       className={classNames(
                         'font-grifter text-xl tablet:text-4xl',
@@ -72,7 +77,13 @@ export default function Roadmap({ className, items }: RoadmapProps) {
                     >
                       {date}
                     </span>
-                    <span className="text-xl">{title}</span>
+                    <span
+                      className={classNames('text-xl line-clamp-2 w-full', {
+                        'font-black': milestone,
+                      })}
+                    >
+                      {title}
+                    </span>
                     {subtitle ? (
                       <span className="font-medium text-sm text-tertiary uppercase">
                         {subtitle}
@@ -83,13 +94,15 @@ export default function Roadmap({ className, items }: RoadmapProps) {
                 {/* dashed line connecting to timeline */}
                 <div
                   className={classNames(
-                    'border-l-4 h-12 w-1 border-dashed my-1.5',
+                    'border-l-4 h-12 w-1 my-1.5',
                     // extra padding to stay outside of circle
                     i % 2 === 0 ? 'mb-2.5' : 'mt-2.5',
                     {
                       'border-l-green-primary': color === 'green-primary',
                       'border-l-orange-primary': color === 'orange-primary',
                       'border-l-purple-primary': color === 'purple-primary',
+                      'border-solid my-0': milestone,
+                      'border-dashed': !milestone,
                     }
                   )}
                 />
@@ -102,19 +115,19 @@ export default function Roadmap({ className, items }: RoadmapProps) {
                       : 'top-0 -translate-y-1/2',
                     'h-6 w-6 border-2 rounded-full bg-dropdown',
                     {
-                      'border-green-primary': color === 'green-primary',
+                      'border-green-primary bg-green-primary':
+                        color === 'green-primary',
                       'border-orange-primary': color === 'orange-primary',
                       'border-purple-primary': color === 'purple-primary',
                     }
                   )}
                 >
-                  <div
-                    className={classNames('h-2 w-2 rounded-full', {
-                      'bg-green-primary': color === 'green-primary',
-                      'bg-orange-primary': color === 'orange-primary',
-                      'bg-purple-primary': color === 'purple-primary',
-                    })}
-                  />
+                  {color === 'green-primary' && (
+                    <IoCheckmarkSharp color="#393b39" />
+                  )}
+                  {color === 'orange-primary' && (
+                    <div className="h-2 w-2 rounded-full bg-orange-primary" />
+                  )}
                 </div>
               </div>
             </div>
--- src/components/pages/about/EngiRoadmap.tsx
+++ src/components/pages/about/EngiRoadmap.tsx
@@ -11,80 +11,121 @@ export default function EngiRoadmap({ className }: EngiRoadmapProps) {
     <div className={classNames('', className)}>
       <div className="max-w-page text-center">
         <h2 className="font-grifter text-4xl sm:text-5xl">Engi Roadmap</h2>
-        <p className="text-lg text-secondary mt-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in
-          elit vel mauris tincidunt porta.
+        <p className="text-lg mt-8 font-light">
+          a chronology in service of the future of work
         </p>
       </div>
       {/* adjust negative margin-right to compensate for extra width from translation */}
-      <div className="mt-8 overflow-x-auto -mr-[370px] tablet:-mr-[400px] scrollbar-horizontal">
+      <div
+        className="mt-8 overflow-x-auto -mr-[370px] tablet:-mr-[400px] scrollbar-horizontal"
+        style={{ width: '100vw' }}
+      >
         <Roadmap
           className="py-32 tablet:py-40"
           items={[
             {
-              date: '2020',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
-              color: 'green-primary',
-            },
-            {
               date: '2021',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              title: 'Initial Commit',
+              subtitle: 'Alpha',
               color: 'green-primary',
             },
             {
               date: '2022',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              title: 'Demonstrate Prototype',
+              subtitle: 'Alpha',
+              color: 'green-primary',
+            },
+            {
+              date: '2022',
+              title: 'Fundraise I',
+              subtitle: 'Alpha',
+              color: 'green-primary',
+            },
+            {
+              date: '2022',
+              title: 'Establish Founding Team',
+              subtitle: 'Alpha',
               color: 'green-primary',
             },
             {
               date: '2023',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              title: 'Launch Contactless Engineering Testnet',
+              subtitle: 'Beta',
+              color: 'green-primary',
+              milestone: true,
+            },
+            {
+              date: '2023',
+              title: 'Open-Source Ecosystem',
+              subtitle: 'Beta',
+              color: 'green-primary',
+            },
+            {
+              date: '2023',
+              title: 'Support Closed-Source Bounties via AI',
+              subtitle: 'Beta',
+              color: 'orange-primary',
+            },
+            {
+              date: '2023',
+              title: 'Launch Kusama Parachain & DAO',
+              subtitle: 'Beta',
+              color: 'orange-primary',
+              milestone: true,
+            },
+            {
+              date: '2023',
+              title: 'Fundraise II',
+              subtitle: 'Beta',
               color: 'orange-primary',
             },
             {
               date: '2024',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
-              color: 'purple-primary',
-            },
-            {
-              date: '2025',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
-              color: 'purple-primary',
-            },
-            {
-              date: '2026',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              title: 'Zero-Knowledge Bounty Verification',
+              subtitle: 'Omega',
               color: 'purple-primary',
             },
             {
               date: '2024',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              title: 'Proof of Engineering Consensus',
+              subtitle: 'Omega',
+              color: 'purple-primary',
+            },
+            {
+              date: '2024',
+              title: 'ICO & Launch Public Mainnet on Polkadot',
+              subtitle: 'Omega',
+              color: 'purple-primary',
+              milestone: true,
+            },
+            {
+              date: '2024',
+              title: 'Fund Grants for Trustless Labor Projects',
+              subtitle: 'Omega',
               color: 'purple-primary',
             },
             {
               date: '2025',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              title: 'Graduate First Engi Center Cohort',
+              subtitle: 'Zeta',
+              color: 'purple-primary',
+            },
+            {
+              date: '2025',
+              title: 'Support All Forms of Engineering Labor',
+              subtitle: 'Zeta',
               color: 'purple-primary',
             },
             {
               date: '2026',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              title: 'Support All Forms of Verifiable Labor',
+              subtitle: 'Zeta',
               color: 'purple-primary',
             },
             {
-              date: '2027',
-              title: 'Technologies Developed',
-              subtitle: 'Academia',
+              date: '2026+',
+              title: '👈 And thats not the half of it...',
+              subtitle: 'Zeta',
               color: 'purple-primary',
             },
           ]}
