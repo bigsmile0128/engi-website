a61dc00110080839e552d154295010d53a224c2f
bigsmile0128 <goldwebnike@gmail.com>
2023-09-25 22:31:10 +0700

    add error handling for expired login on draft creation

--- src/app/(bounty)/hire/[draftId]/error.tsx
+++ src/app/(bounty)/hire/[draftId]/error.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useCallback } from 'react';
 import SignInWithLocalWallets from '~/components/pages/signup/SignInWithLocalWallets';
 
 export default function Error({
@@ -9,17 +10,17 @@ export default function Error({
   error: Error;
   reset: () => void;
 }) {
+  const onLogin = useCallback(() => {
+    // after successfully logging in, reload page
+    window.location.reload();
+  }, []);
+
   return (
     <div>
       <h2 className="font-medium text-xl">
         Your login expired. Please log in again.
       </h2>
-      <SignInWithLocalWallets
-        className="w-full"
-        onSuccess={() => {
-          reset();
-        }}
-      />
+      <SignInWithLocalWallets className="w-full" onSuccess={onLogin} />
     </div>
   );
 }
