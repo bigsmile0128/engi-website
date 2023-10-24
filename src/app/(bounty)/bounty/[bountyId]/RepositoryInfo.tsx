ed899d13754d78bb3c79e7155a5c877a1b74dc82
bigsmile0128 <goldwebnike@gmail.com>
2023-10-25 00:52:50 +0700

    add branch and commit to bounty details page

--- src/app/(bounty)/bounty/[bountyId]/RepositoryInfo.tsx
+++ src/app/(bounty)/bounty/[bountyId]/RepositoryInfo.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
 import Avvvatars from 'avvvatars-react';
-import { RiGithubFill } from 'react-icons/ri';
+import { RiGitBranchFill, RiGitCommitFill, RiGithubFill } from 'react-icons/ri';
 
 type RepositoryInfoProps = {
+  branch?: string;
   className?: string;
+  commit?: string;
   organizationName?: string;
   repositoryName?: string;
 };
@@ -13,11 +15,13 @@ export default function RepositoryInfo({
   className,
   repositoryName,
   organizationName,
+  branch,
+  commit,
 }: RepositoryInfoProps) {
   return (
     <div
       className={classNames(
-        'flex items-center gap-2 overflow-hidden',
+        'flex items-start gap-2 overflow-hidden',
         className
       )}
     >
@@ -28,16 +32,32 @@ export default function RepositoryInfo({
           <div className="h-12 w-12" />
         )}
       </div>
-      <div className="flex flex-col gap-0.5">
+      <div className="flex flex-col gap-0.5 overflow-hidden">
         <span className="font-grifter text-xl inline-block truncate">
           {organizationName ?? 'Author N/A'}
         </span>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 overflow-hidden">
           <RiGithubFill className="h-5 w-5" />
           <span className="text-secondary inline-block truncate">
             {repositoryName ?? 'Repository N/A'}
           </span>
         </div>
+        {branch && (
+          <div className="flex items-center gap-2 overflow-hidden">
+            <RiGitBranchFill className="h-5 w-5" />
+            <span className="text-secondary inline-block truncate">
+              {branch}
+            </span>
+          </div>
+        )}
+        {commit && (
+          <div className="flex items-center gap-2 overflow-hidden">
+            <RiGitCommitFill className="h-5 w-5 shrink-0" />
+            <span className="text-secondary inline-block truncate">
+              {commit}
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
