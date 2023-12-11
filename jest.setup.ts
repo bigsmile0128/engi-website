9619309256751470c335f3407912a39225d42497
bigsmile0128 <goldwebnike@gmail.com>
2023-12-12 07:46:19 +0900

    remove design matcher plugin

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,7 @@
-import { toMatchDesign } from '@engi.network/design-matcher';
+// import { toMatchDesign } from '@engi.network/design-matcher';
 import dotenv from 'dotenv';
 
 dotenv.config();
 // dotenv.config({ path: './.env.development.local' });
 
-expect.extend({ toMatchDesign });
+// expect.extend({ toMatchDesign });
